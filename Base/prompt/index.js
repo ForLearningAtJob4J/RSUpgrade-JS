9de674d52a2841790d13f00fd2b97578948708de
Zergius <sapunov.s@gmail.com>
2025-04-29 01:01:49 +0700

    HelloWorld. Task 4

---
+++ Base/prompt/index.js
@@ -0,0 +1,17 @@
+let answer1 = prompt("Сколько будет 2 + 2?");
+if (answer1 == 4)
+	alert("Верно");
+else
+	alert("Неверно");
+
+let answer2 = prompt("Чему равен корень из 9?");
+if (answer2 == 3)
+	alert("Верно");
+else
+	alert("Неверно");
+
+let answer3 = prompt("Сколько будет 2 в 5-й степени?");
+if (answer1 == 32)
+	alert("Верно");
+else
+	alert("Неверно");
