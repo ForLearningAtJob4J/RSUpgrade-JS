72f6fb152f87f1a0555c82202b5f55bbf45a23f8
Zergius <sapunov.s@gmail.com>
2025-04-29 23:14:42 +0700

    syntax

---
+++ Base/syntax/car.js
@@ -0,0 +1,22 @@
+const carName = "lada-sedan-bakladzhan";
+let maxCarSpeed = 150,
+    carOwner = "Gogi";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 80;
+carOwner = "Nafanya";
+console.log('У машины изменилась скорость - ', maxCarSpeed);
+console.log('У машины изменился владелец - ', carOwner);
+
+maxCarSpeed = 10;
+carOwner = "Luntik";
+console.log('У машины изменилась скорость - ', maxCarSpeed);
+console.log('У машины изменился владелец - ', carOwner);
+
+maxCarSpeed = 500;
+carOwner = "Uporotyi liss";
+console.log('У машины изменилась скорость - ', maxCarSpeed);
+console.log('У машины изменился владелец - ', carOwner);
---
+++ Base/syntax/index.js
@@ -0,0 +1,16 @@
+// JavaScript-код с созданием переменных
+/*
+    Автор:
+    Zerg 
+*/
+let myName = "Sergey",
+    mySurname = "Surname",
+    myFavoriteDrink = "water",
+    myFavoriteAnimal = "dog",
+    myFavoriteProgrammingLanguage = "java(not javascript)";
+    
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
