b5c43e3f2d31c86a53cd73ce6e3e4dc7aa997ea2
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-05-02 00:05:12 +0700

    [TASK] Data types. Task 1

---
+++ Base/data-types/data-types.js
@@ -0,0 +1,17 @@
+const isChecked = true;
+const i = 10;
+const salary = BigInt(10203040506070809010203040506070809010203n);
+const userName = "Zena";
+const undef = undefined;
+const route = null;
+const key = Symbol('id')
+const obj = { name: userName, salary };
+
+console.log('isChecked', isChecked, typeof isChecked);
+console.log('i', i, typeof i);
+console.log('salary', salary, typeof salary);
+console.log('userName', userName, typeof userName);
+console.log('undef', undef, typeof undef);
+console.log('route', route, typeof route);
+console.log('key', key, typeof key);
+console.log('obj', obj, typeof obj);
