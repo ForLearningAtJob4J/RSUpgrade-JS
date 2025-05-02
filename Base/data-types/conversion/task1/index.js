b9cb864aeecf7b3a55f69869a16a72655a8c7e87
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-05-02 21:12:34 +0700

    [TASK] Data types. Conversion. Task 1.

---
+++ Base/data-types/conversion/task1/index.js
@@ -0,0 +1,18 @@
+let hasLegs = true;
+let age = 42;
+let milliSecondsToUniverseDeath = 12121212121212121212121212121212121212121212121212121212121212n;
+let name = 'Dorian';
+let und;
+let smth = null;
+let obj = { left: {}, right: {} };
+let id = Symbol('blablabla');
+
+console.log(Number(hasLegs), Boolean(hasLegs), String(hasLegs));
+console.log(Number(age), Boolean(age), String(age));
+console.log(Number(milliSecondsToUniverseDeath), Boolean(milliSecondsToUniverseDeath), String(milliSecondsToUniverseDeath));
+console.log(Number(name), Boolean(name), String(name));
+console.log(Number(und), Boolean(und), String(und));
+console.log(Number(smth), Boolean(smth), String(smth));
+console.log(Number(obj), Boolean(obj), String(obj));
+//console.log(Number(id));
+console.log(Boolean(id), String(id));
