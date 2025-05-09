d272fa3bc8d2098042ad8c0ac54a235901ee8c51
Zergius <sapunov.s@gmail.com>
2025-05-09 14:49:50 +0700

    [TASK] Strings. Task 1.

---
+++ Base/strings/task1/index.js
@@ -0,0 +1,7 @@
+const myName = 'Сергей';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Владилена Минина';
+const reasonText = 'хочу быть T-shaped';
+const numberOfMonth = 5;
+
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`);
