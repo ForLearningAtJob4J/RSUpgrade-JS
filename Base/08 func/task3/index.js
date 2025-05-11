4f23021e9b90244d62be8e9719a44773ef049678
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-05-11 12:58:47 +0700

    [TASK] 01-08. Task 3.

---
+++ Base/08 func/task3/index.js
@@ -0,0 +1,25 @@
+
+function getDivisorsCount(n) {
+  if (n === undefined || Number(n) === NaN) {
+    return NaN;
+  }
+
+  if (!Number.isInteger(n) || n <= 0) {
+    alert('number должен быть целым положительным числом!')
+    return NaN;
+  }
+
+  let divisorsCount = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      divisorsCount++;
+    }
+  }
+
+  return divisorsCount;
+}
+
+console.log(getDivisorsCount(4)); // Вернет 3 (делители - 1, 2, 4)
+console.log(getDivisorsCount(5)); // Вернет 2 (делители - 1, 5)
+console.log(getDivisorsCount(12)); // Вернет 6 (делители - 1, 2, 3, 4, 6, 12)
+console.log(getDivisorsCount(30)); // Вернет 8 (делители - 1, 2, 3, 5, 6, 10, 15, 30)
