0197d32bacc2c0284e6cd9896c1baa5e46687047
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-05-19 21:31:38 +0700

    [TASK] 01-10. Task 1.

---
+++ Base/10 objects/task1/index.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    username: 'David',
+    status: 'online',
+    lastActivity: 10
+  },
+  {
+    username: 'Lucy',
+    status: 'offline',
+    lastActivity: 22
+  },
+  {
+    username: 'Bob',
+    status: 'online',
+    lastActivity: 104
+  }
+];
+
+const onlineUsers = users.filter((v, i, a) => v.status === 'online');
+
+console.log(`Сейчас в online следующие пользователи: ${onlineUsers.reduce((acc, cur) => acc += cur.username + ', ', '').slice(0, -2)}`);
+
