87bb72a996b0f7efd3ab3f29f791ff5b4ae253fb
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-06-09 22:07:31 +0700

    [TASK] )1-14. Task 2.

---
+++ Base/15 DOM-2/task2/index.js
@@ -0,0 +1,47 @@
+import { tasks, addNewTask, findTaskByText } from './task.js';
+import { tasksDOMRoot, addTaskToDOM } from './DOM.js';
+
+document.addEventListener('DOMContentLoaded',
+    () => {
+        tasksDOMRoot.replaceChildren();
+        tasks.forEach(e => addTaskToDOM(e));
+        document.querySelector('.create-task-block').addEventListener('submit', submitCreateTask);
+        document.querySelector('.create-task-block__input').addEventListener('keypress', () => document.querySelector('.error-message-block')?.remove());
+    }
+);
+
+function submitCreateTask(event) {
+    event.preventDefault();
+    const v = validate(document.querySelector('.create-task-block__input').value);
+
+    document.querySelector('.error-message-block')?.remove();
+
+    if (v) {
+        showError(v);
+        return;
+    }
+
+    addTaskToDOM(
+        addNewTask(document.querySelector('.create-task-block__input').value)
+    );
+}
+
+function validate(text) {
+    if (!text) {
+        return 'Пустое значение!';
+    }
+
+    if (findTaskByText(text)) {
+        return 'Задача уже есть!';
+    }
+
+    return null;
+}
+
+function showError(text) {
+    const errorSpan = document.createElement('span');
+    errorSpan.className = 'error-message-block';
+    errorSpan.textContent = text;
+
+    document.querySelector('.create-task-block').append(errorSpan);
+}
---
+++ Base/15 DOM-2/task2/task.js
@@ -0,0 +1,28 @@
+export const tasks = [
+    {
+        id: '1138465078061',
+        completed: false,
+        text: 'Посмотреть новый урок по JavaScript',
+    },
+    {
+        id: '1138465078062',
+        completed: true,
+        text: 'Выполнить тест после урока',
+    },
+    {
+        id: '1138465078063',
+        completed: false,
+        text: 'Выполнить ДЗ после урока',
+    },
+];
+
+export function addNewTask(text) {
+    const newTask = { id: Date.now(), completed: false, text };
+    tasks.push(newTask);
+    return newTask;
+}
+
+export function findTaskByText(text) {
+    return tasks.some((t) => t.text === text);
+}
+
