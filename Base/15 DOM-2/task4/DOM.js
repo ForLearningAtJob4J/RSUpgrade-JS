17bb8e7dbcb4254a8134015fa96972737aab9ebb
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2025-06-13 19:31:08 +0700

    [TASK] 01-14. Task 4.

---
+++ Base/15 DOM-2/task4/DOM.js
@@ -0,0 +1,62 @@
+export const tasksDOMRoot = document.querySelector('.tasks-list');
+
+function createElementWithClass(owner, tagName, className) {
+    const newElement = owner.createElement(tagName);
+    newElement.className = className;
+    return newElement;
+}
+
+export function addTaskToDOM(task) {
+    const checkboxInput = createElementWithClass(document, 'input', 'checkbox-form__checkbox');
+    checkboxInput.type = 'checkbox';
+    checkboxInput.id = 'task-' + task.id;
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.htmlFor = 'task-' + task.id;
+
+    const checkboxForm = createElementWithClass(document, 'form', 'checkbox-form');
+    checkboxForm.appendChild(checkboxInput);
+    checkboxForm.appendChild(checkboxLabel);
+
+    const taskText = createElementWithClass(document, 'span', 'task-item__text');
+    taskText.textContent = task.text;
+
+    const mainContent = createElementWithClass(document, 'div', 'task-item__main-content');
+    mainContent.appendChild(checkboxForm);
+    mainContent.appendChild(taskText);
+
+    const deleteButton = createElementWithClass(document, 'button', 'task-item__delete-button default-button delete-button');
+    deleteButton.textContent = 'Удалить';
+
+    const taskItem = createElementWithClass(document, 'div', 'task-item');
+    taskItem.setAttribute('data-task-id', task.id);
+
+    const mainContainer = createElementWithClass(document, 'div', 'task-item__main-container');
+    mainContainer.appendChild(mainContent);
+    mainContainer.appendChild(deleteButton);
+
+    taskItem.appendChild(mainContainer);
+    tasksDOMRoot.appendChild(taskItem);
+}
+
+export function deleteTasksByIdsFromDOM(tasksIds) {
+    tasksIds.forEach(taskId => document.querySelector(`[data-task-id="${taskId}"]`)?.remove());
+}
+
+export function addModalWindowToDOM() {
+    document.body.insertAdjacentHTML('beforeend', `
+        <div class="modal-overlay modal-overlay_hidden">
+            <div class="delete-modal">
+                <h3 class="delete-modal__question">
+                    Вы действительно хотите удалить эту задачу?
+                </h3>
+                <div class="delete-modal__buttons">
+                    <button class="delete-modal__button delete-modal__cancel-button">
+                        Отмена
+                    </button>
+                    <button class="delete-modal__button delete-modal__confirm-button">
+                        Удалить
+                    </button>
+                </div>
+            </div>
+        </div>`);
+}
---
+++ Base/15 DOM-2/task4/index.js
@@ -0,0 +1,110 @@
+import { tasks, addNewTask, findTaskByText, deleteTaskById } from './task.js';
+import { tasksDOMRoot, addModalWindowToDOM, addTaskToDOM, deleteTasksByIdsFromDOM } from './DOM.js';
+
+let taskIdToDelete = null;
+let themeIsLight = true;
+
+document.addEventListener('DOMContentLoaded',
+    () => {
+        const initialBackground = document.body.style.background;
+        tasksDOMRoot.replaceChildren();
+        tasks.forEach(e => addTaskToDOM(e));
+
+        addModalWindowToDOM();
+
+        addDeleteButtonHandler();
+        addModalDialogButtonHandler();
+        document.querySelector('.create-task-block').addEventListener('submit', submitCreateTask);
+        document.querySelector('.create-task-block__input').addEventListener('input', () => document.querySelector('.error-message-block')?.remove());
+        document.addEventListener('keydown', toggleThemeHandler);
+    }
+);
+
+function toggleThemeHandler(e) {
+    //console.log(e);
+    if (e.key === 'Tab') {
+        toggleTheme();
+        e.preventDefault();
+    }
+}
+
+function toggleTheme() {
+    themeIsLight = !themeIsLight;
+
+    if (themeIsLight) {
+        document.body.style.background = 'initial';
+        document.querySelectorAll('.task-item').forEach(e => e.style.color = 'initial');
+        document.querySelectorAll('button').forEach(e => e.style.border = 'none');
+    } else {
+        document.body.style.background = ' #24292E';
+        document.querySelectorAll('.task-item').forEach(e => e.style.color = ' #ffffff');
+        document.querySelectorAll('button').forEach(e => e.style.border = '1px solid #ffffff');
+    }
+}
+
+/**** я бы, наверное, все повесил на body, но в задании написано, что эти кнопки надо повесить на .tasks-list */
+function addDeleteButtonHandler() {
+    tasksDOMRoot.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-button')) {
+            taskIdToDelete = e.target.closest('.task-item').dataset.taskId;
+            showModal();
+        }
+    });
+}
+
+function addModalDialogButtonHandler() {
+    document.body.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-modal__cancel-button')) {
+            closeModal();
+        }
+
+        if (e.target.classList.contains('delete-modal__confirm-button')) {
+            deleteTasksByIdsFromDOM(deleteTaskById(taskIdToDelete).map(t => t.id));
+            closeModal();
+        }
+    });
+}
+
+function showModal() {
+    document.querySelector('.modal-overlay').classList.remove('modal-overlay_hidden');
+}
+
+function closeModal() {
+    document.querySelector('.modal-overlay').classList.add('modal-overlay_hidden');
+}
+
+function submitCreateTask(event) {
+    event.preventDefault();
+    const v = validate(document.querySelector('.create-task-block__input').value);
+
+    document.querySelector('.error-message-block')?.remove();
+
+    if (v) {
+        showError(v);
+        return;
+    }
+
+    addTaskToDOM(
+        addNewTask(document.querySelector('.create-task-block__input').value)
+    );
+}
+
+function validate(text) {
+    if (!text) {
+        return 'Название задачи не должно быть пустым';
+    }
+
+    if (findTaskByText(text)) {
+        return 'Задача с таким названием уже существует.';
+    }
+
+    return null;
+}
+
+function showError(text) {
+    const errorSpan = document.createElement('span');
+    errorSpan.className = 'error-message-block';
+    errorSpan.textContent = text;
+
+    document.querySelector('.create-task-block').append(errorSpan);
+}
